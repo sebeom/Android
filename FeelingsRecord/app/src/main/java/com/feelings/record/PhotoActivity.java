324d57ebf52ca8b4e181c33862c2a844ef0223e0
ggangcho <jiyoon101@gmail.com>
2020-03-10 17:37:50 +0900

    앨범 샘플

---
+++ FeelingsRecord/app/src/main/java/com/feelings/record/PhotoActivity.java
@@ -0,0 +1,73 @@
+package com.feelings.record;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class PhotoActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    photoAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.feel_photo);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+
+        GridLayoutManager layoutManager =
+                new GridLayoutManager(this, 3);
+        recyclerView.setLayoutManager(layoutManager);
+
+        adapter = new photoAdapter();
+
+        adapter.addItem(new Person("김민수", "010-1000-1000"));
+        adapter.addItem(new Person("김하늘", "010-2000-2000"));
+        adapter.addItem(new Person("홍길동", "010-3000-3000"));
+        adapter.addItem(new Person("김민수", "010-1000-1000"));
+        adapter.addItem(new Person("김하늘", "010-2000-2000"));
+        adapter.addItem(new Person("홍길동", "010-3000-3000"));
+        adapter.addItem(new Person("김민수", "010-1000-1000"));
+        adapter.addItem(new Person("김하늘", "010-2000-2000"));
+        adapter.addItem(new Person("홍길동", "010-3000-3000"));
+        adapter.addItem(new Person("김민수", "010-1000-1000"));
+        adapter.addItem(new Person("김하늘", "010-2000-2000"));
+        adapter.addItem(new Person("홍길동", "010-3000-3000"));
+        adapter.addItem(new Person("김민수", "010-1000-1000"));
+        adapter.addItem(new Person("김하늘", "010-2000-2000"));
+        adapter.addItem(new Person("홍길동", "010-3000-3000"));
+        adapter.addItem(new Person("김민수", "010-1000-1000"));
+        adapter.addItem(new Person("김하늘", "010-2000-2000"));
+        adapter.addItem(new Person("홍길동", "010-3000-3000"));
+        adapter.addItem(new Person("김민수", "010-1000-1000"));
+        adapter.addItem(new Person("김하늘", "010-2000-2000"));
+        adapter.addItem(new Person("홍길동", "010-3000-3000"));
+        adapter.addItem(new Person("김민수", "010-1000-1000"));
+        adapter.addItem(new Person("김하늘", "010-2000-2000"));
+        adapter.addItem(new Person("홍길동", "010-3000-3000"));
+        adapter.addItem(new Person("김민수", "010-1000-1000"));
+        adapter.addItem(new Person("김하늘", "010-2000-2000"));
+        adapter.addItem(new Person("홍길동", "010-3000-3000"));
+        adapter.addItem(new Person("김민수", "010-1000-1000"));
+        adapter.addItem(new Person("김하늘", "010-2000-2000"));
+        adapter.addItem(new Person("홍길동", "010-3000-3000"));
+
+        recyclerView.setAdapter(adapter);
+
+        adapter.setOnItemClickListener(new OnPersonItemClickListener() {
+            @Override
+            public void onItemClick(photoAdapter.ViewHolder holder, View view, int position) {
+                Person item = adapter.getItem(position);
+                Toast.makeText(getApplicationContext(), "아이템 선택됨: " + item.getName(),
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
