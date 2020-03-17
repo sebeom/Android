9efcd82212ac102d4b7e722b324bb194eaeb830e
sangyoon12 <tkddbs6000@gmail.com>
2020-03-17 15:38:19 +0900

    구글 드라이브 연동

--- FeelingsRecord/app/src/main/java/com/feelings/record/MainActivity.java
+++ FeelingsRecord/app/src/main/java/com/feelings/record/MainActivity.java
@@ -1,19 +1,172 @@
 package com.feelings.record;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+
+import java.util.Collections;
+
+public class MainActivity extends AppCompatActivity{
+    private static final String TAG = "MainActivity";
+
+    private static final int REQUEST_CODE_SIGN_IN = 1;
+    private static final int REQUEST_CODE_OPEN_DOCUMENT =2;
+
+    private DriveServiceHelper mDriveServiceHelper;
+    private String mOpenFileId;
+
+    private Button button;
+    private Button button2;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.backup);
+
+        findViewById(R.id.button).setOnClickListener(view -> openFilePicker());
+        findViewById(R.id.button2).setOnClickListener(view -> createFile());
+
+        requestSignIn();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent resultData){
+        GoogleSignIn.getSignedInAccountFromIntent(resultData).addOnSuccessListener(new OnSuccessListener<GoogleSignInAccount>() {
+            @Override
+            public void onSuccess(GoogleSignInAccount googleSignInAccount)  {
+                Log.i(TAG, "Signed in as " + googleSignInAccount.getEmail());
+
+            }
+        })
+.addOnFailureListener(new OnFailureListener() {
+    @Override
+    public void onFailure(@NonNull Exception e) {
+        Log.e(TAG, "Unable to sign in!", e);
+    }
+});
+        switch (requestCode) {
+            case REQUEST_CODE_SIGN_IN:
+                if (resultCode == Activity.RESULT_OK && resultData != null) {
+                    handleSignInResult(resultData);
+                }
+                break;
+
+            case REQUEST_CODE_OPEN_DOCUMENT:
+                if (resultCode == Activity.RESULT_OK && resultData != null) {
+                    Uri uri = resultData.getData();
+                    if (uri != null) {
+                        openFileFromFilePicker(uri);
+                    }
+                }
+                break;
+        }
+
+        super.onActivityResult(requestCode, resultCode, resultData);
+    }
+
+    private void requestSignIn(){
+Log.d(TAG, "Requesting sign-in");
+        GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().requestScopes(new Scope(DriveScopes.DRIVE_FILE)).build();
+        GoogleSignInClient client = GoogleSignIn.getClient(this, signInOptions);
+
+startActivityForResult(client.getSignInIntent(), REQUEST_CODE_SIGN_IN);
+    }
+
+    private void handleSignInResult(Intent result) {
+        GoogleSignIn.getSignedInAccountFromIntent(result)
+                .addOnSuccessListener(googleAccount -> {
+                    Log.d(TAG, "Signed in as " + googleAccount.getEmail());
+
+                    GoogleAccountCredential credential =
+                            GoogleAccountCredential.usingOAuth2(
+                                    this, Collections.singleton(DriveScopes.DRIVE_FILE));
+                    credential.setSelectedAccount(googleAccount.getAccount());
+                    Drive googleDriveService =
+                            new Drive.Builder(
+                                    AndroidHttp.newCompatibleTransport(),
+                                    new GsonFactory(),
+                                    credential)
+                                    .setApplicationName("Drive API Migration")
+                                    .build();
+
+                    mDriveServiceHelper = new DriveServiceHelper(googleDriveService);
+                })
+                .addOnFailureListener(exception -> Log.e(TAG, "Unable to sign in.", exception));
+    }
+
+    private void openFilePicker() {
+        if (mDriveServiceHelper != null) {
+            Log.d(TAG, "Opening file picker.");
+
+            Intent pickerIntent = mDriveServiceHelper.createFilePickerIntent();
+
+            startActivityForResult(pickerIntent, REQUEST_CODE_OPEN_DOCUMENT);
+        }
+    }
+    private void openFileFromFilePicker(Uri uri) {
+        if (mDriveServiceHelper != null) {
+            Log.d(TAG, "Opening " + uri.getPath());
+
+            mDriveServiceHelper.openFileUsingStorageAccessFramework(getContentResolver(), uri)
+                    .addOnSuccessListener(nameAndContent -> {
+                        String name = nameAndContent.first;
+                        String content = nameAndContent.second;
+
+                        setReadOnlyMode();
+                    })
+                    .addOnFailureListener(exception ->
+                            Log.e(TAG, "Unable to open file from picker.", exception));
+        }
+    }
+
+private void createFile(){
+if(mDriveServiceHelper != null){
+Log.d(TAG, "Creating a file.");
+
+mDriveServiceHelper.createFile().addOnSuccessListener(fileId -> readFile(fileId)).addOnFailureListener(exception -> Log.e(TAG, "Couldn't create file.", exception));
+}
+}
+
+private void readFile(String fileId){
+if(mDriveServiceHelper != null){
+Log.d(TAG, "Reading file " + fileId);
+
+mDriveServiceHelper.readFile(fileId).addOnSuccessListener(nameAndContent -> {
+    String name = nameAndContent.first;
+    String content= nameAndContent.second;
+
+    setReadWriteMode(fileId);
+}).addOnFailureListener(exception -> Log.e(TAG, "Couldn't read file.", exception));
+}
+}
+        private void setReadOnlyMode() {
+            mOpenFileId = null;
+        }
+
+        private void setReadWriteMode(String fileId){
+        mOpenFileId = fileId;
     }
 
     public void back(){ // 뒤로가기 버튼
         super.onBackPressed();
     }
-
 }
