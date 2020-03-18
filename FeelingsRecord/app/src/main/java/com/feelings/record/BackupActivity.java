928edececfe27888e2000f05321dcfe2821f4491
ggangcho <jiyoon101@gmail.com>
2020-03-18 16:48:06 +0900

    글목록 사진 없을때 글만 보이게 하는것 height 200dp 수정

--- FeelingsRecord/app/src/main/java/com/feelings/record/BackupActivity.java
+++ FeelingsRecord/app/src/main/java/com/feelings/record/BackupActivity.java
@@ -37,7 +37,7 @@ public class BackupActivity extends AppCompatActivity {
                 }
             }
         });
-        Button googleBackupButton = findViewById(R.id.googleBackupButton);
+        Button googleBackupButton = findViewById(R.id.button);
         googleBackupButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
